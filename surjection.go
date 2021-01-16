b704ce626015c0008db5fe272aa68fedd8aa13b0
Luc Blassel <luc.blassel@gmail.com>
2021-01-16 11:40:08 +0100

    adding random surjection and mapping generation:

--- surjection.go
+++ surjection.go
@@ -2,9 +2,25 @@ package reductions
 
 import (
 	"errors"
+	"gonum.org/v1/gonum/stat/combin"
+	"math"
 	"math/rand"
+	"sort"
 	"time"
 )
+
+// CountSurjections counts the number of surjections from a set of
+// nStart elements to a set of nEnd elements
+func CountSurjections(nStart, nEnd int) int {
+	res := 0
+	for k:=0; k<=nEnd; k++ {
+		sign := int(math.Pow(-1, float64(nEnd - k)))
+		combs := combin.Binomial(nEnd, k)
+		power := int(math.Pow(float64(k), float64(nStart)))
+		res += sign * combs * power
+	}
+	return res
+}
 
 // RandomMapping returns a random mapping from a input set to an output set
 func RandomMapping(nStart, nEnd int) []int{
@@ -40,4 +56,40 @@ func Surjection(nStart, nEnd int) ([]int, error) {
 		chosen[i] = true
 	}
 	return choices, nil
+}
+
+// GetTuples returns all possible samplings of size n with replacements from an input alphabet
+func GetTuples(n, depth int, universe string, samples []string) []string {
+	if depth == n {
+		return samples
+	}
+	output := make([]string, 0)
+	for _, elem := range samples {
+		for _, char := range universe {
+			output = append(output, elem + string(char))
+		}
+	}
+	if len(samples) == 0 {
+		for _, char := range universe {
+			output = append(output, string(char))
+		}
+	}
+	return GetTuples(n, depth+1, universe, output)
+}
+
+// GetRandomReduction generates a random surjection between a set of input and output sequences
+func GetRandomReduction(inputAlphabet, outputAlphabet  string, inputSize, outputSize int) (map[string]string, error){
+	inputPerms := GetTuples(inputSize, 0, inputAlphabet, []string{})
+	outputPerms := GetTuples(outputSize, 0, outputAlphabet, []string{})
+	sort.Strings(inputPerms)
+	sort.Strings(outputPerms)
+	mapping, err := Surjection(len(inputPerms), len(outputPerms))
+	if err != nil {
+		return nil, err
+	}
+	function := make(map[string]string)
+	for inIdx, outIdx := range mapping {
+		function[inputPerms[inIdx]] = outputPerms[outIdx]
+	}
+	return function, nil
 }
--- surjection_test.go
+++ surjection_test.go
@@ -1,16 +1,19 @@
 package reductions
 
 import (
+	"fmt"
+	"math"
 	"math/rand"
+	"sort"
 	"testing"
 )
 
-func isSurjection(nEnd int, mapping []int) bool{
+func isSurjection(nEnd int, mapping []int) bool {
 	outputs := make(map[int]bool)
 	for _, elem := range mapping {
 		outputs[elem] = true
 	}
-	for i:=0; i<nEnd; i++ {
+	for i := 0; i < nEnd; i++ {
 		if !outputs[i] {
 			return false
 		}
@@ -19,8 +22,8 @@ func isSurjection(nEnd int, mapping []int) bool{
 }
 
 func TestRandomMapping(t *testing.T) {
-	for i:=0; i<100; i++ {
-		start, end := rand.Intn(99) + 1, rand.Intn(99) + 1
+	for i := 0; i < 100; i++ {
+		start, end := rand.Intn(99)+2, rand.Intn(99)+2
 		mapping1, mapping2 := RandomMapping(start, end), RandomMapping(start, end)
 		equal := true
 		for j, elem1 := range mapping1 {
@@ -36,8 +39,8 @@ func TestRandomMapping(t *testing.T) {
 }
 
 func TestSurjection(t *testing.T) {
-	for i:=0; i<100; i++ {
-		start, end := rand.Intn(99) + 1, rand.Intn(99) + 1
+	for i := 0; i < 100; i++ {
+		start, end := rand.Intn(99)+1, rand.Intn(99)+1
 		mapping, err := Surjection(start, end)
 		if end > start {
 			if err == nil {
@@ -49,5 +52,129 @@ func TestSurjection(t *testing.T) {
 				t.Errorf("mapping %v is not a surjectiong of %v to %v", mapping, start, end)
 			}
 		}
+	}
+}
+
+func TestCountSurjections(t *testing.T) {
+	cases := []struct {
+		nStart, nEnd, wanted int
+	}{
+		{nStart: 5, nEnd: 5, wanted: 120},
+		{nStart: 6, nEnd: 5, wanted: 1800},
+		{nStart: 6, nEnd: 6, wanted: 720},
+		{nStart: 7, nEnd: 5, wanted: 16800},
+		{nStart: 7, nEnd: 6, wanted: 15120},
+		{nStart: 7, nEnd: 7, wanted: 5040},
+		{nStart: 8, nEnd: 5, wanted: 126000},
+		{nStart: 8, nEnd: 6, wanted: 191520},
+		{nStart: 8, nEnd: 7, wanted: 141120},
+		{nStart: 8, nEnd: 8, wanted: 40320},
+		{nStart: 9, nEnd: 5, wanted: 834120},
+		{nStart: 9, nEnd: 6, wanted: 1905120},
+		{nStart: 9, nEnd: 7, wanted: 2328480},
+		{nStart: 9, nEnd: 8, wanted: 1451520},
+		{nStart: 9, nEnd: 9, wanted: 362880},
+	}
+	for _, testCase := range cases {
+		t.Run(fmt.Sprintf("%v->%v", testCase.nStart, testCase.nEnd), func(t *testing.T) {
+			if count := CountSurjections(testCase.nStart, testCase.nEnd); count != testCase.wanted {
+				t.Errorf("Got %v expected %v for (%v->%v)", count, testCase.wanted, testCase.nStart, testCase.nEnd)
+			}
+		})
+	}
+}
+
+func TestGetTuples(t *testing.T) {
+	alphabet := "ATGC"
+	cases := []struct{
+		name, universe string
+		n int
+		wanted []string
+	}{
+		{name: "simplest", universe: alphabet, n: 1, wanted:[]string{"A", "C", "G", "T"}},
+		{
+			name: "length2",
+			universe: alphabet,
+			n: 2,
+			wanted:[]string{
+				"AA", "AC", "AG", "AT",
+				"CA", "CC", "CG", "CT",
+				"GA", "GC", "GG", "GT",
+				"TA", "TC", "TG", "TT",
+			},
+		},
+		{
+			name: "length3",
+			universe: alphabet,
+			n: 3,
+			wanted:[]string{
+				"AAA", "AAC", "AAG", "AAT",
+				"ACA", "ACC", "ACG", "ACT",
+				"AGA", "AGC", "AGG", "AGT",
+				"ATA", "ATC", "ATG", "ATT",
+				"CAA", "CAC", "CAG", "CAT",
+				"CCA", "CCC", "CCG", "CCT",
+				"CGA", "CGC", "CGG", "CGT",
+				"CTA", "CTC", "CTG", "CTT",
+				"GAA", "GAC", "GAG", "GAT",
+				"GCA", "GCC", "GCG", "GCT",
+				"GGA", "GGC", "GGG", "GGT",
+				"GTA", "GTC", "GTG", "GTT",
+				"TAA", "TAC", "TAG", "TAT",
+				"TCA", "TCC", "TCG", "TCT",
+				"TGA", "TGC", "TGG", "TGT",
+				"TTA", "TTC", "TTG", "TTT",
+			},
+		},
+	}
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T){
+			perms := GetTuples(testCase.n, 0, testCase.universe, []string{})
+			sort.Strings(perms)
+			if len(perms) != len(testCase.wanted) {
+				t.Errorf("Mismatched lengths: got %v, wanted %v", perms, testCase.wanted)
+			}
+			for i, perm := range perms {
+				if perm != testCase.wanted[i] {
+					t.Errorf("Not the same permutations. Got %v wanted %v", perms, testCase.wanted)
+				}
+			}
+
+		})
+	}
+}
+
+func TestGetRandomReduction(t *testing.T) {
+	inAlph, outAlph := "ATGC", "ATGC."
+	cases := []struct{
+		name, inAlph, outAlph string
+		inSize, outSize int
+	}{
+		{
+			name:"2->1", inAlph: inAlph, outAlph: outAlph, inSize: 2, outSize: 1,
+		},
+		{
+			name:"3->2", inAlph: inAlph, outAlph: outAlph, inSize: 3, outSize: 2,
+		},
+	}
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T){
+			function, err := GetRandomReduction(testCase.inAlph, testCase.outAlph, testCase.inSize, testCase.outSize)
+			if err != nil {
+				t.Errorf("error while getting reduction function: %v", err)
+			}
+			nCases := math.Pow(float64(len(testCase.inAlph)), float64(testCase.inSize))
+			if len(function) != int(nCases) {
+				t.Errorf("Mismatched lengths expected %v got %v", nCases, len(function))
+			}
+			outputs := map[string]bool{}
+			for _,v := range function{
+				outputs[v] = true
+			}
+			nOuts := math.Pow(float64(len(testCase.outAlph)), float64(testCase.outSize))
+			if len(outputs) != int(nOuts) {
+				t.Errorf("Not a surjection, got %v outupts wanted %v", nOuts, len(outputs))
+			}
+		})
 	}
 }
